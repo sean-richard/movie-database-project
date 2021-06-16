81db1b92b6d16f8f8fbd57da6b5932cec6679b13
richardlara35 <richard.u.lara35@gmail.com>
2021-06-16 12:53:28 -0500

    initial

---
+++ js/fetch.js
@@ -0,0 +1,6 @@
+fetch(`https://wide-celestial-board.glitch.me/movies`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error(error));
