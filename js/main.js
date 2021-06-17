5eb3c89d6bf2f192250bc645d6ac2e67997e9a5d
Sean Lewis <sean.lewis018@gmail.com>
2021-06-17 15:26:55 -0500

    movie-style

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ function load() {
     //     $("#loading").html(`<img src="/assets/sunsin4.gif">`);
     getAllMovies();
 }
+
 load();
 
 function domSomething(data) {
@@ -24,16 +25,23 @@ function showMovie(data) {
 
 
         $('#movies').append(`
-    <div class="card col-4">
-    <p class = "card-text text-break"><img src="${poster}"><br>
-    ${movieName}<br>
-    ${genre}<br>
-    ${director}<br>
-    ${actors}<br>
-    "${quote}"<br>
-    ${year}
-</p>
+
+
+
+    <div class="card  col-4 mx-4 mb-4">
+    <img src="${poster}" class="card-img-top-fluid">
+    <div class="card-block">
+    <h5 class="card-header">${movieName}</h5><br>
+    </div>
+    <ul class="list-group list-group-flush"
+    <li class="list-group-item">Genre: ${genre}</li>
+    <li class="list-group-item">Director: ${director}</li>
+    <li class="list-group-item">Cast: ${actors}</li>
+    <li class="list-group-item">"${quote}"</li>
+    <li class="list-group-item">Year: ${year}</li>
+    </ul>
 </div>
-    `)
+
+`)
     }
 }
