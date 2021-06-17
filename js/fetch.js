8d143da6ebd21326171ea7830d46efd1146321dd
richardlara35 <richard.u.lara35@gmail.com>
2021-06-17 10:39:28 -0500

    setup

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,8 @@
-fetch(`https://wide-celestial-board.glitch.me/movies`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => console.error(error));
+function getAllMovies(){
+    fetch(`https://wide-celestial-board.glitch.me/movies`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => console.error(error));
+}
---
+++ js/main.js
@@ -0,0 +1,8 @@
+function load() {
+    alert("Hold Up");
+    // $(document).ready(function(e) {
+    // e.preventDefault()
+    //     $("#loading").html(`<img src="/assets/sunsin4.gif">`);
+    getAllMovies();
+}
+load();
